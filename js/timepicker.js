604685b8d623a150025f2152bff5b51c8892fc1b
Andy Hebrank <ahebrank@gmail.com>
2017-10-10 13:50:09 -0400

    initial commit

---
+++ js/timepicker.js
@@ -0,0 +1,30 @@
+(function($) {
+    // TODO: make the format configurable
+    var format = 'g:i A';
+
+    return Drupal.behaviors.timepicker = {
+      attach: function(context, settings) {
+        if (!Modernizr.inputtypes.time) {
+            var $timefields = $('input[type="time"]');
+            $timefields.timepicker({
+                'timeFormat': format
+            });
+
+            // on submit, convert back to 24-hour time
+            var $forms = $timefields.closest('form');
+            var seen = {};
+            $forms.each(function() {
+                var $form = $(this);
+                if (!seen[$form]) {
+                    $form.on('submit', function() {
+                        $timefields.timepicker({
+                            'timeFormat': 'H:i:s'
+                        });
+                    });
+                }
+                seen[$form] = true;
+            });
+        }
+      }
+    };
+  })(jQuery);
