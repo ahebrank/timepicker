2a6a1d184d67706a883d24c641115e6d17748508
Andy Hebrank <ahebrank@gmail.com>
2018-02-19 17:09:46 -0500

    full input replacement; add moment

--- js/timepicker.js
+++ js/timepicker.js
@@ -1,28 +1,36 @@
 (function($, drupalSettings) {
-    // TODO: make the format configurable
-    var format = 'g:i A';
-
     return Drupal.behaviors.jquery_timepicker = {
       attach: function(context, settings) {
         if (drupalSettings.jquery_timepicker.force_enable || !Modernizr.inputtypes.time) {
             var $timefields = $('input[type="time"]');
-            $timefields.timepicker({
-                'timeFormat': format
-            });
+            $timefields.each(function() {
+                var $timefield = $(this);
+                $timefield.hide();
 
-            // on submit, convert back to 24-hour time
-            var $forms = $timefields.closest('form');
-            var seen = {};
-            $forms.each(function() {
-                var $form = $(this);
-                if (!seen[$form]) {
-                    $form.on('submit', function() {
-                        $timefields.timepicker({
-                            'timeFormat': 'H:i:s'
-                        });
+                // replace the actual time field with a picker-enabled text field
+                var $replace = $('<input type="text" class="form-text" data-timepicker-replacement>');
+                $replace
+                    .insertAfter($timefield)
+                    .timepicker({
+                        'step': 15,
+                        'timeFormat': 'g:ia'
+                    })
+                    .on('change', function() {
+                        var $r = $(this);
+                        var v = $r.val();
+                        var $i = $r.prev();
+                        var f = moment(v, 'h:mma').format('HH:mm:ss');
+                        $i
+                            .val(f)
+                            .attr('value', f);
+
                     });
+
+                // on load
+                var v = $timefield.val();
+                if (v) {
+                    $replace.val(moment(v, 'HH:mm:ss').format('h:mma'));
                 }
-                seen[$form] = true;
             });
         }
       }
